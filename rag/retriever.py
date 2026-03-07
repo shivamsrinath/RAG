e1e08ae1edb299a13f962f2e2fc04f3f4bec2810
shivamsrinath <shivamsrinath@gmail.com>
2026-03-08 03:36:15 +0530

    Retrieve issu fixed

--- rag/retriever.py
+++ rag/retriever.py
@@ -4,13 +4,12 @@ from utils.embeddings import EmbeddingModel
 
 class Retriever:
 
-    def __init__(self, threshold=0.2, top_k=3):
+    def __init__(self, top_k=5):
 
         self.embedding_model = EmbeddingModel().get_model()
         self.vector_store = VectorStore(self.embedding_model)
         self.db = self.vector_store.load()
 
-        self.threshold = threshold
         self.top_k = top_k
 
     def retrieve(self, query):
@@ -18,18 +17,9 @@ class Retriever:
         # BGE query instruction
         query = f"Represent this sentence for searching relevant passages: {query}"
 
-        results = self.db.similarity_search_with_score(
+        docs = self.db.similarity_search(
             query,
             k=self.top_k
         )
 
-        filtered_docs = []
-
-        for doc, score in results:
-
-            similarity = 1 / (1 + score)
-
-            if similarity >= self.threshold:
-                filtered_docs.append(doc)
-
-        return filtered_docs
+        return docs
